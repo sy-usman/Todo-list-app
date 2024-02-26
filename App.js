a22441fcfb55bca6c29886bcdcfaa2df797ea997
Sy-Script <syedusmanbinhasan@gmail.com>
2024-02-26 15:44:20 +0500

    This Project is purely on Javascript.

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
-const input= document.getElementById("input");
-const items= document.getElementById("items-list");
+const input = document.getElementById("input");
+const items = document.getElementById("items-list");
 
-function addtask(){
-    if(input.value === ''){
-        alert("bhai pahlai box ma to kch daal!");
-    }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = input.value;
-        items.appendChild(li);
-    }
+function addtask() {
+  if (input.value === " ") {
+    alert("bhai pahlai box ma to kch daal!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    items.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  input.value = "";
+  browserdata();
 }
+
+items.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("stats");
+      browserdata();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      browserdata();
+    }
+  },
+  false
+);
+
+function browserdata() {
+  localStorage.setItem("data", items.innerHTML);
+}
+function lishow() {
+  items.innerHTML = localStorage.getItem("data");
+}
+
+lishow();
